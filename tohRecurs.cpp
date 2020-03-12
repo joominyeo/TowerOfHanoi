110dd9fe2e90923ebb358fa38c355a1762a5e93a
joominyeo <joominyeo@outlook.com>
2020-03-12 14:44:58 -0700

    added counter and formula calcualtion.

--- tohRecurs.cpp
+++ tohRecurs.cpp
@@ -1,31 +1,34 @@
-// C++ recursive function to  
-// solve tower of hanoi puzzle  
-#include <bits/stdc++.h> 
-using namespace std; 
+#include <math.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+int stepsCounter;
   
-void towerOfHanoi(int n, char from_rod, 
-                    char to_rod, char aux_rod)  
+void towerOfHanoi(int n, char from, char to, char temp)  
 {  
     if (n == 1)  
     {  
-        cout << "Move disk 1 from rod " << from_rod <<  
-                            " to rod " << to_rod<<endl;  
+	stepsCounter++;
+	cout << "Step #" << stepsCounter << ":\t" << "Move disk 1 from rod " << from << " to rod " << to << endl;  
         return;  
     }  
-    towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);  
-    cout << "Move disk " << n << " from rod " << from_rod << 
-                                " to rod " << to_rod << endl;  
-    towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);  
+    towerOfHanoi(n - 1, from, temp, to); //else
+    stepsCounter++;
+    cout << "Step #" << stepsCounter << ":\t" << "Move disk " << n << " from rod " << from << " to rod " << to << endl;  
+    towerOfHanoi(n - 1, temp, to, from);
 }  
   
-// Driver code 
 int main()  
 {  
     string s;
     cout << "Enter n: ";
     getline(cin, s);
     int n;
+    stepsCounter = 0;
     stringstream(s) >> n;  
-    towerOfHanoi(n, 'A', 'C', 'B'); // A, B and C are names of rods  
+    towerOfHanoi(n, 'A', 'C', 'B'); //towers are labeled A,B and C
+    cout << "\nTotal steps by counting:\t" << stepsCounter << "\n" << endl;
+    cout << "By formula (2^n-1):\t" << pow(2.0,n) -1 << "\n" << endl;
+
     return 0;  
 }
